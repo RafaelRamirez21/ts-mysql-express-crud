bc47bef70fe45193ef4d862751251233e11f6f91
Rafael Ramírez <rrramirezb251@gmail.com>
2021-09-27 18:09:34 -0500

    config database

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,12 +2,13 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config()
 const db = new Sequelize(
+
     `${process.env.DB_NAME}`,
     `${process.env.DB_USER}`,
     `${process.env.DB_PASSWORD}`,
    
     {  
-    host:'localhost',
+    host:'us-cdbr-east-04.cleardb.com',
     dialect:'mysql', 
     }
 );
